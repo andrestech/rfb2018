dfc6a279aaed74155fa637220c9d6cb48513541c
andrestech <a.e.s@rambler.ru>
2018-08-12 16:09:25 +0300

    test commit

--- src/main/java/org/andestech/learning/y2018/rfb1/App.java
+++ src/main/java/org/andestech/learning/y2018/rfb1/App.java
@@ -1,11 +1,14 @@
 package org.andestech.learning.y2018.rfb1;
 
+import java.util.Date;
+
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World! " + new Date());
     }
 }
