9c3bc3799fda10137222263d232fe6d8953c04ef
andrestech <a.e.s@rambler.ru>
2018-08-12 14:35:03 +0300

    test commit

---
+++ src/main/java/org/andestech/learning/y2018/rfb1/App.java
@@ -0,0 +1,11 @@
+package org.andestech.learning.y2018.rfb1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
