195398c5aaa2e693aeeaf8679477bd7ebce38aef
andrestech <a.e.s@rambler.ru>
2018-08-12 20:33:23 +0300

    test commit

--- src/main/java/org/andestech/learning/y2018/rfb2/App.java
+++ src/main/java/org/andestech/learning/y2018/rfb2/App.java
@@ -9,6 +9,6 @@ import java.util.Date;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Привет Гит! Hello World! " + new Date());
+        System.out.println("Привет Гитеры! Hello World! " + new Date());
     }
 }
